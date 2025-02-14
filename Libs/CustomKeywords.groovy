1e2b694bfe05cb7b483d9185556d173454de5f29
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2025-02-14 11:05:45 +0700

    Add new .gitignore files and update project settings for better organization

--- Libs/CustomKeywords.groovy
+++ Libs/CustomKeywords.groovy
@@ -3,3 +3,53 @@
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 
+import java.lang.String
+
+
+
+def static "com.testwithhari.katalon.plugins.Gmail.readLatestEMailBodyContent"(
+    	String emailID	
+     , 	String password	
+     , 	String folderLableName	) {
+    (new com.testwithhari.katalon.plugins.Gmail()).readLatestEMailBodyContent(
+        	emailID
+         , 	password
+         , 	folderLableName)
+}
+
+
+def static "com.testwithhari.katalon.plugins.Gmail.deleteAllEMails"(
+    	String emailID	
+     , 	String password	
+     , 	String folderLableName	) {
+    (new com.testwithhari.katalon.plugins.Gmail()).deleteAllEMails(
+        	emailID
+         , 	password
+         , 	folderLableName)
+}
+
+
+def static "com.testwithhari.katalon.plugins.Gmail.getEmailsCount"(
+    	String emailID	
+     , 	String password	
+     , 	String folderLableName	) {
+    (new com.testwithhari.katalon.plugins.Gmail()).getEmailsCount(
+        	emailID
+         , 	password
+         , 	folderLableName)
+}
+
+
+def static "com.testwithhari.katalon.plugins.Gmail.sendEmail"(
+    	String from_emailaddress	
+     , 	String email_password	
+     , 	String to_emailaddress	
+     , 	String email_subject	
+     , 	String email_body	) {
+    (new com.testwithhari.katalon.plugins.Gmail()).sendEmail(
+        	from_emailaddress
+         , 	email_password
+         , 	to_emailaddress
+         , 	email_subject
+         , 	email_body)
+}
